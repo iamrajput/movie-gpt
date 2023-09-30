44055d248220736a993b3d6f78054f20a69a0e48
Atul Kumar Singh <atul.kumar@codelogicx.com>
2023-09-30 18:03:33 +0530

    login page done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import {Provider} from 'react-redux'
+import Body from './componets/Body';
+import appStore from "./utils/appStore";
 
 function App() {
   return (
-    <div className="text-3xl font-bold text-green-400">
-     Hello world
-    </div>
+    <Provider store={appStore}>
+    <Body />
+    </Provider>
   );
 }
 
